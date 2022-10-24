75af32256ad5a9a29df1879f1b27a361688f010d
ShayanMF1 <shayan1227@gmail.com>
2022-10-24 16:20:11 +0100

    init

---
+++ client/src/components/Employees.js
@@ -0,0 +1,17 @@
+const Employees = [
+{
+        id: "1",
+        Name: "Shayan Mojtahedzadeh-Faghihi",
+        Age: "23"
+},{
+    id: "2",
+    Name: "Damon Targeryen",
+    Age: "42"
+},{
+    id: "3",
+    Name: "Jon Snow",
+    Age: "18"
+}
+]
+
+export default Employees;
